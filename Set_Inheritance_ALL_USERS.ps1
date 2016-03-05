29b530292e550ccb2d01ee4dc06136b8a5d17e33
bcprescott <bcprescott@outlook.com>
2016-03-04 21:54:58 -0600

    Powershell scripts for inheritance

---
+++ Set_Inheritance_ALL_USERS.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module ActiveDirectory
+
+$users = Get-ADUser -ldapfilter “(objectclass=user)” -searchbase “dc=MYDOMAIN,dc=COM”
+ForEach($user in $users)
+{
+    # Binding the users to DS
+    $ou = [ADSI](“LDAP://” + $user)
+    $sec = $ou.psbase.objectSecurity
+
+    if ($sec.get_AreAccessRulesProtected())
+    {
+        $isProtected = $false ## allows inheritance
+        $preserveInheritance = $true ## preserves inherited rules
+        $sec.SetAccessRuleProtection($isProtected, $preserveInheritance)
+        $ou.psbase.commitchanges()
+        Write-Host “$user is now inheriting permissions”;
+    }
+    else
+    {
+        Write-Host “$User Inheritable Permission already set”
+    }
+} 
